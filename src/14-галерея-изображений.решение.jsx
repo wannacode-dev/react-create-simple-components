44c579755546d331ca61881527bc94846536be43
Max Roslow <brian2009@yandex.ru>
2026-01-17 17:58:57 +0400

    fix condition

--- src/14-галерея-изображений.решение.jsx
+++ src/14-галерея-изображений.решение.jsx
@@ -92,7 +92,7 @@ function ImageGallery() {
             </div>
             
             {/* Модальное окно */}
-            {selectedImage !== 0 && (
+            {selectedImage !== null && (
                 <div className="modal-overlay" onClick={closeModal}>
                     <div className="modal-content" onClick={(e) => e.stopPropagation()}>
                         <button className="modal-close" onClick={closeModal}>
